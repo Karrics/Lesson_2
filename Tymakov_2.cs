24bde7356e78bf7091eaa65ffa7682ed8748d041
Karrics <adelk4rimov@yandex.ru>
2023-09-18 20:09:28 +0300

    Добавьте файлы проекта.

---
+++ Tymakov_2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tymakov_2
+{
+    internal class Tymakov_2
+    {
+        static void Main(string[] args)
+        {
+            // Спросить имя пользователя и поприветствовать его
+            Console.Write("Упр 2.1:");
+            Console.WriteLine("Спросить имя пользователя и поприветствовать его");
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"здравствуйте, {name}");
+            Console.WriteLine();
+
+            //Деление одного числа на другое 
+            Console.Write("Упр 2.2:");
+            Console.WriteLine("Деление одного числа на другое");
+            Console.WriteLine("Введите целое число");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число");
+            double num2 = double.Parse(Console.ReadLine());
+            if (num2 == 0)
+            {
+                Console.WriteLine("Делить на 0 нельзя");
+            }
+            else
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            Console.WriteLine();
+
+            //Прочитать букву с экрана и вывести на печать следующую за ней
+            //букву в алфавитном порядке.
+            Console.Write("Д.з 2.1:");
+            Console.WriteLine("Вывести следующую букву на экран");
+            Console.WriteLine("Введите одну прописную букву из русского алфавита");
+            char letter = (char)Console.Read();
+            char NextLetter;
+            if (letter == 'я')
+            {
+                NextLetter = 'а';
+            }
+            else
+            {
+                NextLetter = (char)(((int)letter) + 1);
+            }
+            Console.WriteLine(NextLetter);
+            Console.WriteLine();
+
+            //Написать программу, которая решает квадратное уравнение.
+            //Входные данные – коэффициенты уравнения, выходные – найденные корни.
+            Console.Write("Д.з 2.2:");
+            Console.WriteLine("Решить квадратное уравнение");
+            // Тумаков дз 2.2
+            double a, b, c;
+            double d, x1, x2;
+            Console.Write("Введите коэфицент а:");
+            a = Double.Parse(Console.ReadLine());
+            Console.Write("Введите коэфицент b:");
+            b = Double.Parse(Console.ReadLine()); 
+            Console.Write("Введите коэфицент c:"); 
+            c = Double.Parse(Console.ReadLine());
+            d = b * b - 4 * a * c;
+            if (d >= 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                x2 = (-b - Math.Sqrt(d)) / 2 * a;
+                Console.WriteLine(x1);
+                Console.WriteLine(x2);
+            }
+            else
+            {
+                Console.WriteLine("нет решения");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
